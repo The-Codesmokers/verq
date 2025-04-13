862c07097f7f55f1397cc09d0986d9201cb25000
tutankhAman <132681496+tutankhAman@users.noreply.github.com>
2025-04-14 01:03:42 +0530

    landing page first section completed

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'work-sans': ['Work Sans', 'sans-serif'],
+        'montserrat': ['Montserrat', 'sans-serif'],
+      },
+      colors: {
+        'background': '#09090B',
+        'heading': '#E9EAEA',
+        'paragraph': '#A1A1AA',
+      },
+    },
   },
   plugins: [],
 } 
