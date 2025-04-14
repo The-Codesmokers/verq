879c05d2be20e61db2f660c789ab4b99be8694a9
tutankhAman <132681496+tutankhAman@users.noreply.github.com>
2025-04-14 11:37:34 +0530

    iniitialised inteview page, added propoer routes

--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -2,13 +2,15 @@ import { Routes, Route } from 'react-router-dom';
 import Login from '../pages/Login';
 import Register from '../pages/Register';
 import LandingPage from '../pages/LandingPage';
+import Interview from '../pages/interview';
 
 export const AppRoutes = () => {
   return (
     <Routes>
+      <Route path="/" element={<LandingPage />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/" element={<LandingPage />} />
+      <Route path="/interview" element={<Interview />} />
     </Routes>
   );
 }; 
